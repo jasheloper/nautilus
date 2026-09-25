88ba124cb8ee4da519cdf8c1104a92a24d98db6a
Jashele T. <48870048+jasheloper@users.noreply.github.com>
2026-09-24 21:36:52 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+/** Extra Credit:
+ * While not required, if you finish this assignment and have some extra time, feel free to add functionality to the disclaimer button, so that a disclaimer modal pops up when “OFFER DETAILS >” is clicked. (the modal should be closeable) */
+
+
+function offerDetails() {
+const offerDetails = document.querySelector('.offer-details');
+
+offerDetails.addEventListener('click', () => {
+console.log('test');
+});
+}
+
+
+offerDetails();
